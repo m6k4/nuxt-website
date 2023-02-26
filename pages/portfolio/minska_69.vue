931864fdc5f8c62c23edb3372b86d810efa0dcee
Zuzanna Olszewska <zolszewska@apptension.com>
2023-02-26 14:48:18 +0100

    add images for portfolio and prepare portfolio component

--- pages/portfolio/minska_69.vue
+++ pages/portfolio/minska_69.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="Portfolio1">
-    <div class="Portfolio1__parallax" />
-    <div class="Portfolio1__parallax-title">
-      <h1>Mińska 69</h1>
-    </div>
-    <div class="Portfolio1__content">
-      <div class="Portfolio1__description">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+  <PortfolioParallax image="/images/portfolio/bedroom.jpg" title="Mińska 69">
+    <template #content>
+      <div class="Portfolio1__content">
+        <div class="Portfolio1__description">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+        </div>
+        <div class="Portfolio1__grid">
+          <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
+          <img :src="sink" alt="bathroom">
+        </div>
+        <div class="Portfolio1__grid">
+          <img :src="tv" alt="bathroom">
+          <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
+        </div>
+        <div class="Portfolio1__grid">
+          <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
+          <img :src="whiteSink" alt="bathroom">
+        </div>
       </div>
-      <div class="Portfolio1__grid">
-        <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
-        <img :src="sink" alt="bathroom">
-      </div>
-      <div class="Portfolio1__grid">
-        <img :src="tv" alt="bathroom">
-        <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
-      </div>
-      <div class="Portfolio1__grid">
-        <p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever si</p>
-        <img :src="whiteSink" alt="bathroom">
-      </div>
-    </div>
-  </div>
+    </template>
+  </PortfolioParallax>
 </template>
 <script>
+import PortfolioParallax from '~/components/portfolio/PortfolioParallax.vue'
 
 export default {
   name: 'ContactPage',
   components: {
+    PortfolioParallax
   },
   layout: 'page',
   data () {
@@ -49,38 +49,6 @@ export default {
 .Portfolio1 {
   padding: 0 10%;
 
-}
-
-.Portfolio1__parallax {
-  background-image: url('/images/portfolio/bedroom.jpg');
-  min-height: 100vh;
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.Portfolio1__parallax-title {
-  font-family: 'Playfair Display', serif;
-  color: black;
-  font-size: 5rem;
-  line-height: 1rem;
-  padding: 2rem;
-  position: absolute;
-  top: 14%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 5rem;
-  font-weight: 300;
-  text-align: center;
-  z-index: 9;
-  color: #fff
-}
-
-.Portfolio1__parallax-title h1 {
-  margin: 0;
-  padding: 0;
-  font-size: 6rem;
 }
 
 .Portfolio1__content {
