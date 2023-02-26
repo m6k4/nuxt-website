c03aaf73f9d600d2f4f0a1009ce503e63eb85d3f
Zuzanna Olszewska <zolszewska@apptension.com>
2023-02-26 18:01:07 +0100

    aroma park portfolio

---
+++ pages/portfolio/aroma_park.vue
@@ -0,0 +1,94 @@
+<template>
+  <PortfolioParallax image="/images/portfolio/aroma_park/aroma_park5.jpg" title="Aroma Park">
+    <template #content>
+      <div class="Portfolio2__content">
+        <div class="Portfolio2__description">
+          <p>Projekt apartamentu zlokalizowanego na warszawskiej Białołęce w inwestycji Aroma Park. Mimo niedużej powierzchni lokalu, dzięki niewielkim zmianom układu pomieszczeń, udało się zrealizować bardzo funkcjonalną i spójną wizualnie przestrzeń. Wnętrze jest wyraziste i kontrastowe a jednocześnie kameralne i przytulne. We wszystkich pomieszczeniach panuje podobna kolorystyka – dominują czerń i szarości, a duża ilość drewnianych wykończeń w różnej postaci ocieplają wnętrze.</p>
+        </div>
+        <div class="Portfolio2_image-container">
+          <img :src="image6" alt="living-room" class="Portfolio2__img">
+        </div>
+        <div class="Portfolio2_image-container">
+          <div class="Portfolio2__row">
+            <img :src="image5" alt="living-room" class="Portfolio2__img">
+            <img :src="image2" alt="living-room" class="Portfolio2__img">
+          </div>
+          <div class="Portfolio2__row">
+            <img :src="image3" alt="kitchen" class="Portfolio2__img">
+            <img :src="image4" alt="kitchen" class="Portfolio2__img">
+          </div>
+        </div>
+        <div class="Portfolio2__description">
+          <p>W pomieszczeniu sypialni również postawiono na praktyczność i maksymalne wykorzystanie przestrzeni do przechowywania. Duża zabudowa meblowa szafy przechodząca w zabudowę meblową na ścianie za łóżkiem została zaprojektowana w czarnym kolorze z wykorzystaniem naturalnego forniru drewnianego. Fornir stanowi również wykończenie wnęk pełniących funkcję szafek nocnych, a przytulności dodaje wysokie tapicerowane wezgłowie w układzie jodełki. Na bocznych ścianach wnęki nad łóżkiem wykorzystano, spójnie komponujące się z całością aranżacji, kinkiety marki Chors.</p>
+        </div>
+        <div class="Portfolio2_image-container">
+          <img :src="image1" alt="bedroom" class="Portfolio2__img">
+        </div>
+      </div>
+    </template>
+  </PortfolioParallax>
+</template>
+<script>
+import PortfolioParallax from '~/components/portfolio/PortfolioParallax.vue'
+
+export default {
+  name: 'PortfolioSinglePage',
+  components: {
+    PortfolioParallax
+  },
+  layout: 'page',
+  data () {
+    return {
+      image1: '/images/portfolio/aroma_park/aroma_park1.jpg',
+      image2: '/images/portfolio/aroma_park/aroma_park2.jpg',
+      image3: '/images/portfolio/aroma_park/aroma_park3.jpg',
+      image4: '/images/portfolio/aroma_park/aroma_park4.jpg',
+      image5: '/images/portfolio/aroma_park/aroma_park5.jpg',
+      image6: '/images/portfolio/aroma_park/aroma_park6.jpg'
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+.Portfolio2 {
+  padding: 0 10%;
+
+}
+
+.Portfolio2__img{
+  width: 100%;
+}
+
+.Portfolio2__description {
+  padding: 0 10%;
+  margin: 10rem 0;
+}
+
+.Portfolio2__row {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem 0;
+  gap: 8px;
+}
+
+.Portfolio2__row img{
+  width: 48%;
+}
+
+  @media only screen and (max-width: 750px) {
+    .Portfolio2__row {
+      flex-direction: column;
+      gap: 2rem;
+    }
+
+    .Portfolio2__row img{
+      width: 100%;
+    }
+
+    .Portfolio2__description {
+      padding: 0;
+      margin: 2rem 0;
+    }
+  }
+</style>
--- pages/portfolio/ursus.vue
+++ pages/portfolio/ursus.vue
@@ -1,48 +1,48 @@
 <template>
-    <PortfolioParallax image="/images/portfolio/ursus/ursus4.jpg" title="Ursus">
-        <template #content>
-        <div class="Portfolio1__content">
-            <div class="Portfolio1__description">
-            <p>Ursus – niegdyś dzielnica Warszawy o typowo industrialnym charakterze, kojarzona z licznymi zakładami produkcyjnymi. Dziś jest to miejsce w trakcie przemiany, która postępuje w ekspresowym tępie. Dzielnica staje się przyjazną i zaplanowaną urbanistycznie przestrzenią miejską ze zdecydowaną przewagą zabudowy mieszkaniowej.</p>
-            <p>Wnętrze mieszkania zostało zaprojektowane w stylu loftowym. Industrialnego charakteru nadały wnętrzu takie materiały jak płytki z cegły rozbiórkowej oraz tynki imitujące beton. Akcenty stanowią dodatki w czarnych kolorach, a cała aranżacja została ocieplona poprzez zastosowanie drewna w wykończeniu mebli, blatu kuchennego i okładzin ściennych.</p>
-            </div>
-            <div class="Portfolio1_image-container">
-            <img :src="image2" alt="bathroom" class="Portfolio1__img">
-            </div>
-            <div class="Portfolio1_image-container">
-            <div class="Portfolio1__row">
-                <img :src="image1" alt="bathroom" class="Portfolio1__img">
-                <img :src="image3" alt="bathroom" class="Portfolio1__img">
-            </div>
-            </div>
-            <div class="Portfolio1__description">
-            <p>W realizacji inwestycji wykorzystano oświetlenie belgijskiej marki Wever & Ducre. Szklane, dymione klosze  skomponowane z trzech rozmiarów lamp wiszących serii Wetro nadaje części salonowej troche 'błysku’, natomiast lampy z serii Roomor zawieszone nad blatem kuchennym utrzymują swoją formą i matowym wykończeniem charakter wnętrza.</p>
-            </div>
-            <div class="Portfolio1_image-container">
-            <img :src="image4" alt="bathroom" class="Portfolio1__img">
-            </div>
-            <div class="Portfolio1_image-container" style="padding: 2.5rem 0;">
-            <img :src="image5" alt="bathroom" class="Portfolio1__img">
-            </div>
-            <div class="Portfolio1_image-container">
-            <img :src="image6" alt="bathroom" class="Portfolio1__img">
-            </div>
-            <div class="Portfolio1__description">
-            <p>Nieocenione stały się jednak lampy Roomor na sztywnym połączeniu zwieszane nad szafkami nocnymi w pomieszczeniu sypialni. Ich forma idealnie wpisała się koncepcję zabudowy meblowej jaka została przedstawiona przez klientów w wytycznych do realizacji projektu..</p>
-            </div>
-            <div class="Portfolio1_image-container">
-            <img :src="image7" alt="bathroom" class="Portfolio1__img">
-            </div>
-            <div class="Portfolio1_image-container">
-            <div class="Portfolio1__row">
-                <img :src="image8" alt="bathroom" class="Portfolio1__img">
-                <img :src="image9" alt="bathroom" class="Portfolio1__img">
-            </div>
-            </div>
+  <PortfolioParallax image="/images/portfolio/ursus/ursus4.jpg" title="Ursus">
+    <template #content>
+      <div class="Portfolio1__content">
+        <div class="Portfolio1__description">
+          <p>Ursus – niegdyś dzielnica Warszawy o typowo industrialnym charakterze, kojarzona z licznymi zakładami produkcyjnymi. Dziś jest to miejsce w trakcie przemiany, która postępuje w ekspresowym tępie. Dzielnica staje się przyjazną i zaplanowaną urbanistycznie przestrzenią miejską ze zdecydowaną przewagą zabudowy mieszkaniowej.</p>
+          <p>Wnętrze mieszkania zostało zaprojektowane w stylu loftowym. Industrialnego charakteru nadały wnętrzu takie materiały jak płytki z cegły rozbiórkowej oraz tynki imitujące beton. Akcenty stanowią dodatki w czarnych kolorach, a cała aranżacja została ocieplona poprzez zastosowanie drewna w wykończeniu mebli, blatu kuchennego i okładzin ściennych.</p>
         </div>
-        </template>
-    </PortfolioParallax>
+        <div class="Portfolio1_image-container">
+          <img :src="image2" alt="bathroom" class="Portfolio1__img">
+        </div>
+        <div class="Portfolio1_image-container">
+          <div class="Portfolio1__row">
+            <img :src="image1" alt="bathroom" class="Portfolio1__img">
+            <img :src="image3" alt="bathroom" class="Portfolio1__img">
+          </div>
+        </div>
+        <div class="Portfolio1__description">
+          <p>W realizacji inwestycji wykorzystano oświetlenie belgijskiej marki Wever & Ducre. Szklane, dymione klosze  skomponowane z trzech rozmiarów lamp wiszących serii Wetro nadaje części salonowej troche 'błysku’, natomiast lampy z serii Roomor zawieszone nad blatem kuchennym utrzymują swoją formą i matowym wykończeniem charakter wnętrza.</p>
+        </div>
+        <div class="Portfolio1_image-container">
+          <img :src="image4" alt="bathroom" class="Portfolio1__img">
+        </div>
+        <div class="Portfolio1_image-container" style="padding: 2.5rem 0;">
+          <img :src="image5" alt="bathroom" class="Portfolio1__img">
+        </div>
+        <div class="Portfolio1_image-container">
+          <img :src="image6" alt="bathroom" class="Portfolio1__img">
+        </div>
+        <div class="Portfolio1__description">
+          <p>Nieocenione stały się jednak lampy Roomor na sztywnym połączeniu zwieszane nad szafkami nocnymi w pomieszczeniu sypialni. Ich forma idealnie wpisała się koncepcję zabudowy meblowej jaka została przedstawiona przez klientów w wytycznych do realizacji projektu..</p>
+        </div>
+        <div class="Portfolio1_image-container">
+          <img :src="image7" alt="bathroom" class="Portfolio1__img">
+        </div>
+        <div class="Portfolio1_image-container">
+          <div class="Portfolio1__row">
+            <img :src="image8" alt="bathroom" class="Portfolio1__img">
+            <img :src="image9" alt="bathroom" class="Portfolio1__img">
+          </div>
+        </div>
+      </div>
     </template>
+  </PortfolioParallax>
+</template>
 <script>
 import PortfolioParallax from '~/components/portfolio/PortfolioParallax.vue'
 
