4346cec4dc0424041d1104c01c472ae1642719b1
Zuzanna Olszewska <zolszewska@apptension.com>
2023-03-11 22:21:09 +0100

    fix animation

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -7,12 +7,12 @@
       alt="background image with living room"
       :style="isLoading ? 'opacity: 1' : 'opacity: 0'"
     />
-    <div class="MainPage__title" :style="isLoading ? 'top: 2rem' : 'top: 0'">
+    <div class="MainPage__title" :class="{ 'show': isLoading }">
       <p>przestrzen.</p>
       <p>swiatlo.</p>
       <p>proporcja.</p>
     </div>
-    <div :class="`MainPage__content ${!isLoading && 'MainPage__content-animate'}`">
+    <div class="MainPage__content" :class="{ 'show': isLoading }">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
     </div>
   </div>
@@ -62,11 +62,21 @@ export default {
   color: black;
   position: absolute;
   left: 8rem;
+  top: 4rem;
   text-transform: uppercase;
   font-size: 4.5vw;
   line-height: 1rem;
-  transition: all 1.5s ease-in-out
+  transition: all 1.5s ease-in-out;
+  transform: translateY(-100%);
+  opacity: 0;
 }
+
+.MainPage__title.show {
+  transform: translateY(0);
+  opacity: 1;
+  transition-delay: 0.5s;
+}
+
 .MainPage__content {
   position: absolute;
   bottom: 15rem;
@@ -75,12 +85,14 @@ export default {
   font-weight: 500;
   text-transform: uppercase;
   transition: all 1.5s ease-in-out;
-  right: 5rem ;
-  opacity: 1
-}
-.MainPage__content-animate {
-  right: 0;
+  right: 1rem ;
+  transform: translateX(100%);
   opacity: 0;
+}
+.MainPage__content.show {
+  transform: translateX(0);
+  opacity: 1;
+  transition-delay: 0.5s;
 }
 @media only screen and (max-width: 1600px) {
   .MainPage__content {
